977eb120c3501ba8a9f2e94df7a15d3d0a135df4
ImperaZim <92953608+ImperaZim@users.noreply.github.com>
2022-06-22 01:37:34 -0300

    update WarpCommand.php

--- src/iWarp/Commands/WarpCommand.php
+++ src/iWarp/Commands/WarpCommand.php
@@ -4,22 +4,22 @@ namespace iWarp\Commands;
 
 use iWarp\Loader;
 
-use pocketmine\plugin\{
- Plugin, 
+use pocketmine\plugin\ {
+ Plugin,
  PluginOwned
 };
-use pocketmine\command\{
-  Command,
-  PluginCommand,
-  CommandSender
-}; 
-use pocketmine\{
- utils\Config,
- player\Player, 
+use pocketmine\command\ {
+ Command,
+ PluginCommand,
+ CommandSender
+};
+use pocketmine\ {
+ Utils\Config,
+ player\Player,
 };
 
 class WarpCommand extends Command {
- 
+
  private $pl;
 
  public function __construct($main) {
@@ -32,80 +32,80 @@ class WarpCommand extends Command {
   $plugin = $this->pl;
   $player = $plugin->getServer()->getPlayerExact($player->getName());
   $config = new Config($plugin->getDataFolder() . "warps.yml");
-  $warps = $config->getAll(); 
-  
+  $warps = $config->getAll();
+
   $message = $plugin->getConfig();
-  
+
   $permission = $message->get("command.permission");
   $permissiable = $message->get("command.permission");
-  
-  if($player->hasPermission($permission) == true){
+
+  if ($player->hasPermission($permission) == true) {
+   $permission = "true";
+  } else {
+   $permission = "false";
+   if ($permissiable == "default") {
     $permission = "true";
-   }else{
-    $permission = "false";
-    if($permissiable == "default"){
-     $permission = "true";
-    }
-    if($plugin->getServer()->isOp($player->getName()) == true){
-     $permission = "true";
-    }
-   } 
-  
+   }
+   if ($plugin->getServer()->isOp($player->getName()) == true) {
+    $permission = "true";
+   }
+  }
+
   if ($player instanceof Player) {
    if (isset($args[0])) {
     switch ($args[0]) {
-     case "tp": case "teleport": 
-      if(isset($args[1])){
+     case "tp": case "teleport":
+      if (isset($args[1])) {
        $plugin->getWarpEvents()->teleport($player, $args[1]);
-      }else{
+      } else {
        $player->sendMessage($message->get("command.teleport.help")); #OK
       }
       break;
-     case "set": case "setar":
-      if ($permission == "false") {
-        $player->sendMessage($message->get("command.nopermission")); #OK
-        return true;
-       } 
-      if (isset($args[1])) {
-       $warp = $args[1];
-       $world = $player->getWorld()->getDisplayName();
-       $x = (int)$player->getPosition()->getX();
-       $y = (int)$player->getPosition()->getY();
-       $z = (int)$player->getPosition()->getZ();
-       $permission = null;
-       if (isset($args[2])) $permission = $args[2];
-       $plugin->getWarpEvents()->create($player, $warp, $world, [$x, $y, $z], $permission, "");
-      } else {
-       $player->sendMessage($message->get("command.set.help")); #OK
-      }
-      break;
-      case "del": case "delete":
+      case "set": case "setar":
        if ($permission == "false") {
         $player->sendMessage($message->get("command.nopermission")); #OK
         return true;
        }
        if (isset($args[1])) {
         $warp = $args[1];
-        $plugin->getWarpEvents()->delete($player, $warp);
+        $world = $player->getWorld()->getDisplayName();
+        $x = (int)$player->getPosition()->getX();
+        $y = (int)$player->getPosition()->getY();
+        $z = (int)$player->getPosition()->getZ();
+        $permission = null;
+        if (isset($args[2])) $permission = $args[2];
+        $plugin->getWarpEvents()->create($player, $warp, $world, [$x, $y, $z], $permission, "");
        } else {
-        $player->sendMessage($message->get("command.del.help") ); #OK
+        $player->sendMessage($message->get("command.set.help")); #OK
        }
        break;
-       case "list": case "lista":
-        $plugin->getWarpEvents()->list($player);
+       case "del": case "delete":
+        if ($permission == "false") {
+         $player->sendMessage($message->get("command.nopermission")); #OK
+         return true;
+        }
+        if (isset($args[1])) {
+         $warp = $args[1];
+         $plugin->getWarpEvents()->delete($player, $warp);
+        } else {
+         $player->sendMessage($message->get("command.del.help")); #OK
+        }
         break;
+        case "list": case "lista":
+         $plugin->getWarpEvents()->list($player);
+         break;
+       }
+     } else {
+      if ($permission == "true" || $plugin->getServer()->isOp($player->getName()) == true) {
+       $player->sendMessage($message->get("command.help.op")); #OK
+      } else {
+       $player->sendMessage($message->get("command.help.player")); #OK
       }
-    } else {
-     if($permission == "true" || $plugin->getServer()->isOp($player->getName()) == true){
-      $player->sendMessage($message->get("command.help.op")); #OK
-     }else{
-      $player->sendMessage($message->get("command.help.player")); #OK
      }
+    } else {
+     $plugin->getLogger()->warning("Esté comando só está disponivel no servidor!");
     }
-   } else {
-    $plugin->getLogger()->warning("Esté comando só está disponivel no servidor!");
+    return true;
    }
-   return true;
-  }
 
- }
+  }

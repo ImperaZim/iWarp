1488b42d637e19e3a39cce1b5a312133714a4b55
ImperaZim <92953608+ImperaZim@users.noreply.github.com>
2022-06-22 01:11:45 -0300

    Create WarpDelete.php

---
+++ src/iWarp/Eventos/WarpEvents/WarpDelete.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace iWarp\Eventos\WarpEvents;
+
+use iWarp\Loader;
+use pocketmine\Utils\Config; 
+
+class WarpDelete {
+ 
+ private $warp;
+ 
+ public function __construct($player, $warp) {
+  $plugin = Loader::getInstance();
+  $config = new Config($plugin->getDataFolder() . "warps.yml");
+  $warps = $config->getAll();
+  $data = $plugin->getDataFolder();
+  
+  $message = $plugin->getConfig();  
+  
+  if(!isset($warps[$warp])){
+   $player->sendMessage($plugin->getProcessedTags($message->get("command.del.nowarp"), $warp));
+  }else{
+   unset($warps[$warp]);
+   $config->setAll($warps); 
+   $config->save(); 
+   $player->sendMessage("\n§l§bWARP§r A warp §b{$warp}§r foi deletada com sucesso!\n §a"); 
+   $player->sendMessage($plugin->getProcessedTags($message->get("command.del.sucess"), $warp));
+  } 
+ }
+ 
+}

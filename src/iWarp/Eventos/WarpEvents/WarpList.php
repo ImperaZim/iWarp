16186db80a370eede01a9ee3dcb35dee3ece2497
ImperaZim <92953608+ImperaZim@users.noreply.github.com>
2022-06-22 01:11:17 -0300

    Create WarpList.php

---
+++ src/iWarp/Eventos/WarpEvents/WarpList.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace iWarp\Eventos\WarpEvents;
+
+use iWarp\Loader;
+use pocketmine\Utils\Config;
+
+class WarpList {
+ 
+ private $user;
+
+ public function __construct($user) {
+  $player = $user;
+  $plugin = Loader::getInstance();
+  $config = new Config($plugin->getDataFolder() . "warps.yml");
+  $config = $config->getAll();
+  
+  $message = $plugin->getConfig(); 
+  
+  $warps = "";
+  foreach ($config as $warp => $name){
+   $name = ", §b{$warp}§7"; $warps .= "$name";
+  }
+  $warps = substr($warps, 2);
+  
+  if(count($config) <= 0) {
+   $player->sendMessage($plugin->getProcessedTags($message->get("command.list.nowarps"), $warps)); 
+   return;
+  }
+  
+  $player->sendMessage($plugin->getProcessedTags($message->get("command.list.warps"), $warps)); 
+  
+ }
+
+} 

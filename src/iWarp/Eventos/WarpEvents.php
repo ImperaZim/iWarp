9ba7c34038650005b76d93d2847159caf1f35d3c
ImperaZim <92953608+ImperaZim@users.noreply.github.com>
2022-06-22 01:07:36 -0300

    Create WarpEvents.php

---
+++ src/iWarp/Eventos/WarpEvents.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace iWarp\Eventos;
+
+use iWarp\Loader;
+use pocketmine\Utils\Config;
+
+use iWarp\Eventos\WarpEvents\{
+ WarpTeleport,
+ WarpCreation, 
+ WarpDelete,
+ WarpList,
+};
+
+class WarpEvents {
+ 
+ public function __construct() {
+  $plugin = Loader::getInstance(); 
+  $config = new Config($plugin->getDataFolder() . "warps.yml"); 
+  return $config;
+  }
+ 
+ public function create($player, $warp, $world, $position, $permission = null, $path = "") {
+  return new WarpCreation($player, $warp, $world, $position, $permission, $path);
+ }
+ 
+ public function delete($player, $warp) { 
+  return new WarpDelete($player, $warp); 
+ }
+ 
+ public function teleport($player, $warp) { 
+  return new WarpTeleport($player, $warp); 
+ }
+ 
+ public function list($player) { return new WarpList($player); }
+ 
+}

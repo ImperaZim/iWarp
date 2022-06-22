a5e56838a9cd132528a6a398067f4c0efb431e6c
ImperaZim <92953608+ImperaZim@users.noreply.github.com>
2022-06-22 01:12:14 -0300

    Create WarpCreation.php

---
+++ src/iWarp/Eventos/WarpEvents/WarpCreation.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace iWarp\Eventos\WarpEvents;
+
+use iWarp\Loader;
+use pocketmine\Utils\Config;
+
+class WarpCreation {
+
+ private $warp,
+ $path,
+ $world,
+ $position,
+ $permission;
+
+ public function __construct($player, $warp, $world, $position, $permission, $path) {
+  $plugin = Loader::getInstance();
+  $config = new Config($plugin->getDataFolder() . "warps.yml");
+  $warps = $config->getAll();
+  $data = $plugin->getDataFolder();
+  
+  $message = $plugin->getConfig(); 
+
+  if (isset($warps[$warp])) {
+   $player->sendMessage($plugin->getProcessedTags($message->get("command.set.haswarp"), $warp));
+  } else {
+   $config = new Config($data . "warps.yml", Config::YAML, [
+    "$warp" => [
+     "name" => "$warp",
+     "world" => "$world",
+     "position" => [
+      "PosX" => "$position[0]", 
+      "PosY" => "$position[1]", 
+      "PosZ" => "$position[2]"
+     ],
+     "permission" => "$permission",
+     "path" => "$path",
+    ],
+   ]);
+   $config->save();
+   $player->sendMessage($plugin->getProcessedTags($message->get("command.set.sucess"), $warp)); 
+  }
+
+ }
+
+}
